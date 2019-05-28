430782aeaedb597e7c091a7e49629ca1a43bb20d
Andrew Tack <andrew.tack.recapllc@gmail.com>
2019-05-28 17:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  let theFunk = "FUNKY!"
   return function() {
     return "FUNKY!"
   }
@@ -23,4 +22,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction ()
